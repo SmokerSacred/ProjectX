fb8567c9b9486dbdc137286dffeb049aad18808f
SmokerSacred <thed3vil69@gmail.com>
2026-04-14 16:24:01 +0300

    Working on  kitchen printer logic

--- src/populate.py
+++ src/populate.py
@@ -8,11 +8,24 @@ cols_defaults = {
     }
 
 def autofill(df):
-    Filled_Vals = []
+    filled_vals = []
 
     for default_value, columns in cols_defaults.items():
         for column in columns:
-          blank_mask = df[column].isnull() | df[column].astype(str).str.strip() == ""
+            blank_mask = df[column].isnull() | (df[column].astype(str).str.strip() == "")
+            filled_mask = ~blank_mask
 
+            for idx in df.index[filled_mask]:
+                filled_vals.append((idx, column, df.at[idx, column]))
 
-        return
+            df.loc[blank_mask, column] = default_value
+
+    for j in df["PrintOnKot"]:
+        if j == "Yes":
+            for i in df["Category"]:
+                if i == "FOOD" or i == "Food" or i == "food":
+                    df.loc[idx, "KitchenPrinter"] = "KOT"
+                elif i == "BEVERAGE" or i == "Beverage" or i == "beverage":
+                    df.loc[idx, "KitchenPrinter"] = "BOT"
+
+    return filled_vals, df
